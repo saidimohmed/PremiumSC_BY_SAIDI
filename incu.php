2c5c6ada05964c8c04c9a915fd8ca443c529a25d
saidimohmed <101113676+saidimohmed@users.noreply.github.com>
2022-03-23 19:00:07 +0100

    Add files via upload

---
+++ incu.php
@@ -0,0 +1,56 @@
+<div class="ST-item" style="margin-top:5px">
+	<div class="ST-subitem" style="--i:6;">
+	<img class="jujurlahpadakujujurlahpadaku" src="https://g.top4top.io/p_22732ac840.png">
+		<div class="ST-claim" onclick="stFunc(this)">Claim</div>
+	</div>
+	<div class="ST-subitem" style="--i:5;">
+	<img class="jujurlahpadakujujurlahpadaku" src="https://h.top4top.io/p_22731govr1.png">
+		<div class="ST-claim" onclick="stFunc(this)">Claim</div>
+	</div>
+	<div class="ST-subitem" style="--i:4;">
+	<img class="jujurlahpadakujujurlahpadaku" src="https://i.top4top.io/p_2273zugbd2.png">
+		<div class="ST-claim" onclick="stFunc(this)">Claim</div>
+	</div>
+</div>
+<div class="ST-item">
+	<div class="ST-subitem" style="--i:3;">
+	<img class="jujurlahpadakujujurlahpadaku" src="https://j.top4top.io/p_2273fkpho3.png">
+		<div class="ST-claim" onclick="stFunc(this)">Claim</div>
+	</div>
+	<div class="ST-subitem" style="--i:2;">
+	<img class="jujurlahpadakujujurlahpadaku" src="https://k.top4top.io/p_2273jq7mz4.png">
+		<div class="ST-claim" onclick="stFunc(this)">Claim</div>
+	</div>
+	<div class="ST-subitem" style="--i:1;">
+	<img class="jujurlahpadakujujurlahpadaku" src="https://l.top4top.io/p_2273qjud15.png">
+		<div class="ST-claim" onclick="stFunc(this)">Claim</div>
+	</div>
+</div>
+<div class="ST-item">
+	<div class="ST-subitem" style="--i:1;">
+	<img class="jujurlahpadakujujurlahpadaku" src="https://a.top4top.io/p_2273vvivn6.png">
+		<div class="ST-claim" onclick="stFunc(this)">Claim</div>
+	</div>
+	<div class="ST-subitem" style="--i:2;">
+	<img class="jujurlahpadakujujurlahpadaku" src="https://b.top4top.io/p_227323sez7.png">
+		<div class="ST-claim" onclick="stFunc(this)">Claim</div>
+	</div>
+	<div class="ST-subitem" style="--i:3;">
+	<img class="jujurlahpadakujujurlahpadaku" src="https://c.top4top.io/p_2273do3hw8.png">
+		<div class="ST-claim" onclick="stFunc(this)">Claim</div>
+	</div>
+</div>
+<div class="ST-item">
+	<div class="ST-subitem" style="--i:4;">
+	<img class="jujurlahpadakujujurlahpadaku" src="https://d.top4top.io/p_227319jhx9.png">
+		<div class="ST-claim" onclick="stFunc(this)">Claim</div>
+	</div>
+	<div class="ST-subitem" style="--i:5;">
+	<img class="jujurlahpadakujujurlahpadaku" src="https://a.top4top.io/p_2273moqce0.png">
+		<div class="ST-claim" onclick="stFunc(this)">Claim</div>
+	</div>
+	<div class="ST-subitem" style="--i:6;">
+	<img class="jujurlahpadakujujurlahpadaku" src="https://b.top4top.io/p_22733gyfo1.png">
+		<div class="ST-claim" onclick="stFunc(this)">Claim</div>
+	</div>
+</div>
